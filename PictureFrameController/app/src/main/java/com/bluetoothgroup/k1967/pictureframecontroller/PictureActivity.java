30ae2f8e496d44d1a2c011e57dfb80fbd9fb5267
Valtteri Seuranen <K1967@Labranet.jamk.fi>
2017-11-14 10:54:02 +0200

    new activity

--- PictureFrameController/app/src/main/java/com/bluetoothgroup/k1967/pictureframecontroller/PictureActivity.java
+++ PictureFrameController/app/src/main/java/com/bluetoothgroup/k1967/pictureframecontroller/PictureActivity.java
@@ -1,13 +1,44 @@
 package com.bluetoothgroup.k1967.pictureframecontroller;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class PictureActivity extends AppCompatActivity {
+
+    private ImageController mImageController;
+    private static final int IMAGECAPTURE_TAG = 664;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture);
+
+        mImageController = new ImageController(this);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == IMAGECAPTURE_TAG)
+        {
+            Log.i("IMAGE_ACTIVITY_RESULT", "Got response of take image activity");
+            if(resultCode == RESULT_OK)
+            {
+                //Image has been taken successfully
+                Log.e("IMAGE_ACTIVITY_RESULT", "Taking image has succeeded");
+
+                //get image from intent
+                Bundle bundle = data.getExtras();
+                Bitmap image = (Bitmap)bundle.get("data");
+            }
+            else
+            {
+                //Image has failed
+                Log.e("IMAGE_ACTIVITY_RESULT", "Taking image has failed");
+            }
+        }
+    }
+
 }
